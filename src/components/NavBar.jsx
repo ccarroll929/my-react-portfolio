26873aed3129988682d50596f9617ce77b3063d1
Christina Carroll <ccarroll0528@gmail.com>
2024-04-16 18:55:30 -0500

    created navbar component and compiled images/icons in src/img

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react"; 
-import {Nav, Navbar, Container} from 'react-bootstrap/Container';
-
+import logo from '../assets/img/logo.svg';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const navBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -27,7 +32,7 @@ export const navBar = () => {
 return (
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
     <Container>
-        <Navbar.Brand href="#home"><img src={''} alt="Logo" /></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} alt="Logo" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
@@ -39,9 +44,9 @@ return (
             </Nav>
         <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
-                <a href="#"><img src={''} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="#"><img src={navIcon3} alt="" /></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect!</span></button>
         </span>
