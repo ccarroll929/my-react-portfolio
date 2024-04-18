aed25195edd722908f3e3191ef7d1745cc909278
Christina Carroll <ccarroll0528@gmail.com>
2024-04-18 13:41:26 -0500

    created banner/main page body, styled the navbar and main body

---
+++ src/components/Banner.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.png";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+// import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+    const period = 2000;
+
+useEffect(() => {
+    let ticker = setInterval(() => {
+    tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+}, [text])
+
+const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+    setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+    setIsDeleting(true);
+    setIndex(prevIndex => prevIndex - 1);
+    setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+    setIsDeleting(false);
+    setLoopNum(loopNum + 1);
+    setIndex(1);
+    setDelta(500);
+    } else {
+    setIndex(prevIndex => prevIndex + 1);
+    }
+}
+
+return (
+    <section className="banner" id="home">
+    <Container>
+        <Row className="align-items-center">
+        <Col xs={12} md={6} xl={7}>
+            <div className="headerImg">
+                <img src={headerImg} alt="Header Img"/>
+            </div>
+        </Col>
+        <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+            {({ isVisible }) =>
+            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <h1>{`Hi! I'm Christina:`}</h1>
+                <span className="txt-rotate" dataperiod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span>
+                <p>Welcome to my portfolio page! My name is Christina Carroll and I'm a developer with a passion for problem solving and creating unique web designs. Feel free to explore my portfolio and checkout my skills, projects, resume, and social links!</p>
+                <button className="connect" onClick={() => console.log('connect')}><a href="#socials">Let’s Connect</a><ArrowRightCircle size={25} /></button>
+            </div>}
+            </TrackVisibility>
+        </Col>
+        </Row>
+    </Container>
+    </section>
+)
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react"; 
-import logo from '../assets/img/logo.svg';
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import { useState, useEffect } from "react"; 
+import { Navbar, Nav, Container } from "react-bootstrap";
+import socialIcon1 from '../assets/img/linkedin_icon.png';
+import socialIcon2 from '../assets/img/github_icon.png';
+import socialIcon3 from '../assets/img/gmail_icon.png';
+import {BrowserRouter as Router} from "react-router-dom";
 
-export const navBar = () => {
+export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
@@ -29,29 +27,29 @@ export const navBar = () => {
         setActiveLink(value);
     }
 
-return (
-    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-    <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="Logo" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+    return (
+        <Router>
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+            <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+                <span className="navbar-toggler-icon"></span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                <span className="navbar-text">
                 <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link' } onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects]')}>Projects</Nav.Link>
-            </Nav>
-        <span className="navbar-text">
-            <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-            </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect!</span></button>
-        </span>
-        </Navbar.Collapse>
-        </Container>
-    </Navbar>
-);
-}
+                <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+                <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                </span>
+                </Nav>
+                <div className="social-icon">
+                    <a href="#"><img src={socialIcon1} alt="linkedin icon"></img></a>
+                    <a href="#"><img src={socialIcon2} alt="github icon"></img></a>
+                    <a href="#"><img src={socialIcon3} alt="gmail icon"></img></a>
+                </div>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </Router>
+    )
+    }
