9270435b802505aa890fef3e1a516fa1e87a114d
Christina Carroll <ccarroll0528@gmail.com>
2024-04-18 13:46:25 -0500

    imported NavBar and Banner as elements to be rendered in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+// import { Skills } from "./components/Skills";
+// import { Projects } from "./components/Projects";
+// import { Contact } from "./components/Contact";
+// import { Footer } from "./components/Footer";
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-  <div className="App">
-  </div>
-  )
+    <div className="App">
+      <NavBar />
+      <Banner className="banner"/>
+      {/* <Skills />
+      <Projects />
+      <Contact />
+      <Footer /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
