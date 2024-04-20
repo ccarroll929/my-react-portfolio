f33d866b7cd1b162873f8351f3f293a319de756c
Christina Carroll <ccarroll0528@gmail.com>
2024-04-20 17:43:02 -0500

    added new Footer content to body

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
 function App() {
   const [load, upadateLoad] = useState(true);
 
@@ -24,6 +26,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
