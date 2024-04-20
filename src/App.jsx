c548c43c06900bf4bb79c4e085f9f2de6669bf73
Christina Carroll <ccarroll0528@gmail.com>
2024-04-19 21:08:14 -0500

    imported Project element into App.jsx and appended it to the body

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -21,8 +22,8 @@ function App() {
       <NavBar />
       <Banner className="banner"/>
       <Skills />
-      {/* <Skills />
       <Projects />
+      {/*
       <Contact />
       <Footer /> */}
     </div>
