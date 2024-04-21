c65e6a6b23b6b5cebf9638b13a01cd87e72c919f
Christina Carroll <ccarroll0528@gmail.com>
2024-04-21 17:41:50 -0500

    modified navbar to highlight active navigation link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import {BrowserRouter as Router} from "react-router-dom";
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
+    const [activeLink, setActiveLink] = useState('aboutme');
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
@@ -27,18 +27,23 @@ export const NavBar = () => {
     return (
         <Router>
         <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <span className="navbar-toggler-icon"></span>
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="navbar">
                 <span className="navbar-text">
-                <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                <Nav.Link href="#aboutme" className={activeLink === 'aboutme' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('aboutme')}>About Me</Nav.Link>
                 <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                 <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+                <Nav.Link href="#resume" className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('resume')}>Resume</Nav.Link>
+                <Nav.Link href="#contact" className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('contact')}>Contact</Nav.Link>
+                <Nav.Link href="#socials" className={activeLink === 'socials' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('socials')}>Socials</Nav.Link>
                 </span>
                 </Nav>
             </Navbar.Collapse>
+            </Container>
         </Navbar>
         </Router>
     )
